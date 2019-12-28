7f2d2eecd47e398193b4b300062de7da5f969e73
sonihiten5 <41137951+sonihiten5@users.noreply.github.com>
2019-12-28 22:39:53 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ if(isset($_POST['submit']))
     $email = $_POST('email');
     $subject = $_POST('subject');
     $message = $_POST('message');
-    $to = "sonihiten5@gmail.com";
+    $to = 'sonihiten5@gmail.com';
     $headers = "From: ".$email;
     $message = "Name: ".$name. "\n\n". "Email:".$email." \n\n". "subject:".$subject."\n\n". "message:".$message."\n\n";
    mail($to,$subject,$message,$headers)
