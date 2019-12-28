17875fe328cc842054f0f6cfa441b1c8115abfd8
sonihiten5 <41137951+sonihiten5@users.noreply.github.com>
2019-12-28 22:57:20 +0530

    Add files via upload

---
+++ mail-handler.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $message=$_POST['message'];
+    $to='sonihiten5@gmail.com';
+    $headers="From: ".$email;
+    $message="Name: ".$name. "\n\n". "Email:".$email." \n\n". "subject:".$subject."\n\n". "message:".$message;
+  if( mail($to,$subject,$message,$headers))
+  {
+      echo .$name."we will contact you soon";
+  }
+    else
+    {
+        echo "something went wrong";
+    }
+ 
+}
+?>
