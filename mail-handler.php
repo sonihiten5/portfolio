69573f61b218f5239bd66e9c0d7f73eba290cad2
sonihiten5 <41137951+sonihiten5@users.noreply.github.com>
2019-12-29 21:28:28 +0530

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -1,21 +1,15 @@
 <?php
 if(isset($_POST['submit']))
 {
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $subject=$_POST['subject'];
-    $message=$_POST['message'];
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   $subject=$_POST['subject'];
+   $message=$_POST['message'];
     $to='sonihiten5@gmail.com';
-   $headers="From: ".$email;
-  $message="Name: ".$name. "\n\n". "Email:".$email." \n\n". "subject:".$subject."\n\n". "message:".$message;
-  if(mail($to,$subject,$message,$headers))
-  {
-      echo .$name."we will contact you soon";
-  }
-    else
-    {
-        echo "something went wrong";
-    }
- 
+    $headers="From: ".$email;
+   $message1="Name: ".$name. "\n\n". "Email:".$email." \n\n". "subject:".$subject."\n\n". "message:".$message;
+  mail($to,$subject,$message1);
+      echo "we will contact you soon";
+      //echo $name,$email,$subject,$message;
 }
 ?>
